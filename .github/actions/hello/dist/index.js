7ea681a6b9381b6caaa1a1e203fba5a7fc11876d
feedforwardsystems <feedforwardsystems@gmail.com>
2023-03-19 09:19:29 -0400

    action update

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -2114,11 +2114,19 @@ module.exports = /******/ (function (modules, runtime) {
       const github = __webpack_require__(469);
 
       try {
+        core.debug("debug message");
+        core.warning("warning message");
+        core.error("error message");
         const name = core.getInput("who-to-greet");
+        console.log(`Hello ${name}`);
+        core.setSecret(name);
         console.log(`Hello ${name}`);
         const time = new Date();
         core.setOutput("time", time.toTimeString());
+        core.startGroup("github github object");
         console.log(JSON.stringify(github, null, "\t"));
+        core.endGroup();
+        core.exportVariable("HI", "--hi--");
       } catch (error) {
         core.setFailed(error.message);
       }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,11 +2,19 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  core.debug("debug message");
+  core.warning("warning message");
+  core.error("error message");
   const name = core.getInput("who-to-greet");
+  console.log(`Hello ${name}`);
+  core.setSecret(name);
   console.log(`Hello ${name}`);
   const time = new Date();
   core.setOutput("time", time.toTimeString());
+  core.startGroup("github github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+  core.exportVariable("HI", "--hi--");
 } catch (error) {
   core.setFailed(error.message);
 }
